5dc3c1a078dc60904a6c42fb570261443d274060
Harrison Fung <harrison.hok.fung@gmail.com>
2023-04-19 14:42:19 -0400

    removed routes folder and removed some routes from server.js

--- server.js
+++ server.js
@@ -5,11 +5,6 @@ const app = express();
 // use the express-static middleware
 app.use(express.static("public"));
 
-app.get('/about.html', (req, res) => {
-    res.sendFile(__dirname + '/about.html');
-});
-
-
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
